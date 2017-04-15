459dfadb5a31580978777639c2c9c333252cdabc
thecodegator <thecodegator@gmail.com>
2017-04-15 23:34:18 +0530

    Initial commit

---
+++ RSSreddit/RSSreddit.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from xml.etree import ElementTree
+import re
+import json
+
+
+class RSSreddit(object):
+
+    subreddit = "http://www.reddit.com/.rss"
+    tree = None
+
+    def __init__(self, url=subreddit):
+
+        response = requests.get(url, headers={'User-agent': 'RSSreddit'})
+        print(response)
+        data = ElementTree.fromstring(response.content)
+        print(self.recurse(data))
+
+        # return self.dati
+
+    def recurse(self, element):
+        dati = {}
+        for elem in element:
+            item = self.tag_name(elem)
+            print(item)
+            if type(elem.text) is str:
+                dati[item] = elem.text
+            elif elem.attrib:
+                if item in dati:
+                    dati[item].append(elem.attrib)
+                else:
+                    dati[item] = [elem.attrib]
+            else:
+                if item in dati:
+                    dati[item].append(self.recurse(elem))
+                else:
+                    dati[item] = [self.recurse(elem)]
+        return dati
+        # self.block = {}
+        # for elem in element:
+        #     item = self.tag_name(elem)
+        #     # print(item)
+        #     if type(elem.text) is str:
+        #         self.block[item] = elem.text
+        #     elif elem.attrib:
+        #         if item in self.block:
+        #             temp, self.block[item] = self.block[item], []
+        #             self.block[item].append(temp)
+
+
+    def __str__(self):
+        print(str(RSSreddit.tree))
+
+    def tag_name(self, tag):
+        return re.findall(r'}(\w+)', tag.tag)[0] or None
---
+++ RSSreddit/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .RSSreddit import RSSreddit
+
+__author__ = """Algogator"""
+__email__ = 'algogator@gmail.com'
+__version__ = '0.1.0'
