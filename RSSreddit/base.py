9be22affed1d600aeaf58f664d91491d457b5347
thecodegator <thecodegator@gmail.com>
2017-04-23 22:19:02 +0530

    Add categories

--- RSSreddit/base.py
+++ RSSreddit/base.py
@@ -3,7 +3,12 @@ from .util import getXML, recurseXML
 class BaseRSS(object):
     # URL = "http://www.reddit.com"
     def __init__(self, rss):
-        # print(rss+"/.rss")
-        self.xml = getXML(rss+"/.rss")
+        self.URL = rss
+
+    def make_request(self, arg):
+        self.xml = getXML(self.URL+arg+"/.rss")
         if self.xml:
-            self.json = recurseXML(self.xml)
+            return recurseXML(self.xml)
+
+    def comments(self):
+        return self.make_request("comments")
--- RSSreddit/rssreddit.py
+++ RSSreddit/rssreddit.py
@@ -5,9 +5,23 @@ class subreddit(BaseRSS):
 
     """Generate a subreddit instance."""
 
-    def __init__(self, rss, category = ""):
-        BaseRSS.__init__(self, ("http://www.reddit.com/r/{0}/{1}").format(rss,category))
+    def __init__(self, rss):
+        BaseRSS.__init__(self, ("http://www.reddit.com/r/{0}/").format(rss))
 
+    def hot(self):
+        return self.make_request("hot")
+
+    def top(self, arg):
+        return self.make_request("top")
+
+    def controversial(self, arg):
+        return self.make_request("controversial")
+
+    def new(self, arg):
+        return self.make_request("new")
+
+    def gilded(self, arg):
+        return self.make_request("gilded")
 
 class user(BaseRSS):
 
